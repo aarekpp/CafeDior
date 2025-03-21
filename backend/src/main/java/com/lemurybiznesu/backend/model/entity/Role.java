698a6f58b320d1406b83f5910092cca9a5574a5b
Arkadiusz Przywara <62338732+aarekpp@users.noreply.github.com>
2025-03-21 23:44:47 +0100

    Adding role model

--- backend/src/main/java/com/lemurybiznesu/backend/model/entity/Role.java
+++ backend/src/main/java/com/lemurybiznesu/backend/model/entity/Role.java
@@ -1,26 +1,47 @@
 package com.lemurybiznesu.backend.model.entity;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import org.hibernate.annotations.NaturalId;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "roles")
+@Table(name = "roles",
+        indexes = @Index(name = "idx_role_name", columnList = "name"),
+        uniqueConstraints = @UniqueConstraint(name = "uc_role_name", columnNames = "name"))
 @EntityListeners(AuditingEntityListener.class)
+@EqualsAndHashCode(of = "name")
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(updatable = false, nullable = false, columnDefinition = "BIGINT UNSIGNED AUTO_INCREMENT")
     private Long id;
 
-    @NotBlank
-    @Column(nullable = false, unique = true)
-    private String name;
+    @NotNull(message = "Role name cannot be null")
+    @Size(min = 20, max = 20)
+    @Enumerated(EnumType.STRING)
+    @NaturalId
+    @Column(name = "name", nullable = false, unique = true, updatable = false, length = 20,
+            columnDefinition = "VARCHAR(20) CHECK (name IN ('ROLE_USER','ROLE_MODERATOR','ROLE_ADMIN'))")
+    private ERole name;
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false, nullable = false,
+            columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP")
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false,
+            columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
+    private LocalDateTime updatedAt;
 }
--- backend/src/main/java/com/lemurybiznesu/backend/repository/RoleRepository.java
+++ backend/src/main/java/com/lemurybiznesu/backend/repository/RoleRepository.java
@@ -1,5 +1,6 @@
 package com.lemurybiznesu.backend.repository;
 
+import com.lemurybiznesu.backend.model.entity.ERole;
 import com.lemurybiznesu.backend.model.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,5 +9,6 @@ import java.util.Optional;
 
 @Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
-    Optional<Role> findByName(String name);
+    Optional<Role> findByName(ERole name);
+    boolean existsByName(ERole name);
 }
