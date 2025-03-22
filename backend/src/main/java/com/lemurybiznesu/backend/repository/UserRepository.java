b6e778187ee79bcf33ece6a27a588f0525c8e294
Arkadiusz Przywara <62338732+aarekpp@users.noreply.github.com>
2025-03-22 01:13:23 +0100

    adding user repository and service
    removing role service

---
+++ backend/src/main/java/com/lemurybiznesu/backend/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.lemurybiznesu.backend.repository;
+
+import com.lemurybiznesu.backend.model.entity.Role;
+import com.lemurybiznesu.backend.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+    List<User> findByRole(Role role);
+}
