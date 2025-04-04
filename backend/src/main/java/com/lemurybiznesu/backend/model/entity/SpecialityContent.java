3523847ab9a3b3d2ed8d13963c1264d0296afaa5
Arkadiusz Przywara <62338732+aarekpp@users.noreply.github.com>
2025-04-04 19:09:24 +0200

    new model
    speciality content secion model

--- backend/src/main/java/com/lemurybiznesu/backend/model/entity/SpecialityContent.java
+++ backend/src/main/java/com/lemurybiznesu/backend/model/entity/SpecialityContent.java
@@ -3,6 +3,7 @@ package com.lemurybiznesu.backend.model.entity;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
@@ -15,19 +16,33 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "about_content")
+@Table(name = "specialities_content", indexes = {@Index(name = "idx_specialities_order", columnList = "display_order")})
 @EntityListeners(AuditingEntityListener.class)
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-public class AboutContent {
+public class SpecialityContent {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     @Column(updatable = false, nullable = false, columnDefinition = "BINARY(16)")
     @EqualsAndHashCode.Include
     private String id;
 
-    @NotBlank(message = "Text cannot be blank")
+    @NotBlank(message = "Main title cannot be blank")
+    @Size(max = 100)
+    @Column(name = "main_title", nullable = false, length = 100, columnDefinition = "VARCHAR(100) COLLATE utf8mb4_unicode_ci")
+    private String mainTitle;
+
+    @NotBlank(message = "Second title cannot be blank")
+    @Size(max = 200)
+    @Column(name = "second_title", length = 200, columnDefinition = "VARCHAR(200) COLLATE utf8mb4_unicode_ci")
+    private String secondTitle;
+
+    @NotBlank(message = "Description cannot be blank")
     @Column(nullable = false, columnDefinition = "TEXT COLLATE utf8mb4_unicode_ci")
-    private String text;
+    private String description;
+
+    @NotNull(message = "Display order cannot be empty")
+    @Column(name = "display_order", nullable = false, columnDefinition = "INT UNSIGNED")
+    private Integer displayOrder;
 
     @NotNull
     @ManyToOne(fetch = FetchType.LAZY)
@@ -47,6 +62,8 @@ public class AboutContent {
     @PrePersist
     @PreUpdate
     private void normalizeData() {
-        this.text = text.trim();
+        this.mainTitle = mainTitle.trim();
+        this.secondTitle = secondTitle.trim();
+        this.description = description.trim();
     }
 }
