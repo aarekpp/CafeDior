3b0b894a80af5346ee09fe247009a80b08b8bacb
Arkadiusz Przywara <62338732+aarekpp@users.noreply.github.com>
2025-04-12 11:56:48 +0200

    home controller

--- backend/src/main/java/com/lemurybiznesu/backend/service/ImageContentService.java
+++ backend/src/main/java/com/lemurybiznesu/backend/service/ImageContentService.java
@@ -29,7 +29,7 @@ public class ImageContentService {
     }
 
     public List<ImageContent> getAllImages() {
-        return new ArrayList<>();
+        return imageContentRepository.findAll();
     }
 
     @Transactional
